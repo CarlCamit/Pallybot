29e15bee235380bc6886fb04795ae59fe046178a
Carl Camit <carlcamit0@gmail.com>
2018-12-30 22:37:14 +1100

    Create Client (#9)
    Make a function to create new instances of a client with different oauth
    keys and safe transport settings.

--- api/lifx.go
+++ api/lifx.go
@@ -1,7 +1,9 @@
 package lifx
 
 import (
+	"net"
 	"net/http"
+	"time"
 )
 
 // Client specifies the settings for communicating with the LIFX API
@@ -11,4 +13,29 @@ type Client struct {
 	// Re-use the same client so TCP connections can be cached
 	// http.Client is safe for re-use across goroutines
 	client *http.Client
+}
+
+// NewClient returns a Client instance
+func NewClient(hostURL string, oauthKey string) *Client {
+	transport := &http.Transport{
+		// http.DefaultTransport values
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+
+		// Custom values
+		MaxIdleConns:        1,
+		MaxIdleConnsPerHost: 1,
+	}
+
+	return &Client{
+		hostURL:  hostURL,
+		oauthKey: oauthKey,
+		client:   &http.Client{Transport: transport},
+	}
 }
