5ea822e2fde1d5f0b4f19758166d52e037604cb8
Carl Camit <carlcamit0@gmail.com>
2019-03-31 12:58:56 +1100

    WIP

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+const pongMessage = "PONG :tmi.twitch.tv\r\n"
+
 func main() {
 	fmt.Printf("[%s] Connecting to Twitch IRC...\n", time.Now())
 
@@ -51,25 +53,48 @@ func main() {
 			rawMessage := bytes.NewBuffer(messageBytes).String()
 			// Check if message is a heartbeat
 			if rawMessage == "PING :tmi.twitch.tv\r\n" {
-				chatResponse := "PONG :tmi.twitch.tv\r\n"
-				fmt.Printf("%s", chatResponse)
-				conn.WriteMessage(1, []byte(chatResponse))
+				conn.WriteMessage(1, []byte(pongMessage))
+				fmt.Printf("%s", pongMessage)
 				// Move on to next message after responding
 				continue
 			}
 
-			rawMessageSlice := strings.Split(rawMessage, " ")
 			// Check if the message is a chat message
-			if rawMessageSlice[1] != "PRIVMSG" {
+			if strings.Contains(rawMessage, "PRIVMSG") == false {
 				// Move on to next message if not a chat message
 				continue
 			}
 
+			rawMessageSlice := strings.Split(strings.TrimSpace(rawMessage), " ")
 			chatMessageSlice := strings.Split(rawMessageSlice[3], " ")
 			// Check if message is a command
 			if strings.HasPrefix(chatMessageSlice[0], ":!") {
-				command := strings.TrimPrefix(chatMessageSlice[0], ":!")
-				fmt.Printf("%s", command)
+				switch chatMessageSlice[0] {
+				case ":!twitter":
+					conn.WriteMessage(1, []byte("PRIVMSG #bewitchedpixels :/me Follow Pix on Twitter to get updates for when she goes live! twitter.com/BewitchedPixels\r\n"))
+					continue
+				case ":!tumblr":
+					conn.WriteMessage(1, []byte("PRIVMSG #bewitchedpixels :/me Check out Pix's Tumblr here: bewitchedpixels.tumblr.com\r\n"))
+					continue
+				case ":!insta":
+					conn.WriteMessage(1, []byte("PRIVMSG #bewitchedpixels :/me Check out Pix's Instagram here: instagram.com/bewitchedpixels\r\n"))
+					continue
+				case ":!social":
+					conn.WriteMessage(1, []byte("PRIVMSG #bewitchedpixels :/me ALL THE THINGS: instagram.com/bewitchedpixels || bewitchedpixels.tumblr.com || twitter.com/BewitchedPixels\r\n"))
+					continue
+				case ":!donate":
+					conn.WriteMessage(1, []byte("PRIVMSG #bewitchedpixels :/me Donations very much appreciated, but are of course never required, and are non-refundable https://twitch.streamlabs.com/bewitchedpixels\r\n"))
+					continue
+				case ":!animals":
+					conn.WriteMessage(1, []byte("PRIVMSG #bewitchedpixels :/me Pix has 2 dogs. Eduardo is a 10 year old Black long-haired Chi/Pom. Pixel is a 11 year old Tawny short-haired Chihuahua.\r\n"))
+					continue
+				case ":!discord":
+					conn.WriteMessage(1, []byte("PRIVMSG #bewitchedpixels :/me Come join our cult of awesome witches and wizards: https://discord.gg/aFtpg7R\r\n"))
+					continue
+					// case ":!blind":
+					// 	conn.WriteMessage(1, []byte("PRIVMSG #bewitchedpixels :/me Hey guys! This is my first playthrough so please avoid providing spoilers which includes adding emotional context to an upcoming part, and no tips or tricks. If I need any help I will let you guys know, thank you and enjoy!!!\r\n"))
+					// 	continue
+				}
 			}
 		}
 	}()
